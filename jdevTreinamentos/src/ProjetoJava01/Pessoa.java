c0ffc83a9f00ce551fcb518adadcc63d95fe6f9a
islopes <islopes@LBR-PE07P5J4.corp.capgemini.com>
2024-05-24 17:00:55 -0300

    Alteração e criação da classe pessoa

---
+++ jdevTreinamentos/src/ProjetoJava01/Pessoa.java
@@ -0,0 +1,26 @@
+package ProjetoJava01;
+
+public class Pessoa {
+	
+	private String nome;
+	
+	private String sobreNome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobreNome() {
+		return sobreNome;
+	}
+
+	public void setSobreNome(String sobreNome) {
+		this.sobreNome = sobreNome;
+	}
+	
+
+}
